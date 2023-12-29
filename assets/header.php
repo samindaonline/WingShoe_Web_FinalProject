b8223edf729d89b7673981139f66372d148539c3
Saminda Lakshan <sam.lakshanz@gmail.com>
2023-12-29 17:55:31 +0530

    Initial commit

---
+++ assets/header.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+        <link rel="stylesheet" href="css_files/nav.css">
+    <header>
+        <div class="container">
+            <nav>
+                <div class="imglogo"><img src="assets/logo/WingShoelogo.png" alt="logo"></div>
+                <div class="navlist">
+                    <ul>
+                        <li><a href="index.php">Home</a></li>
+                        <li><a href="#">Shop</a></li>
+                        <li><a href="#">About</a></li>
+                        <li><a href="#">Contact</a></li>     
+                    </ul>
+                </div>
+                <div class="navleft">
+                    <div class="account">
+                        <div class="accgroup">
+                            <div class="accicon"></div>
+                            <div class="accname"></div>
+                        </div>
+                    </div>
+                    <div class="cart">
+                        <span class="cartcount">0</span>
+                        <img src="assets/icon/shoppingcart.png" alt="cart">
+                    </div>
+                </div>
+            </nav>
+        </div>
+</header>
---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Home</title>
+        <link rel="stylesheet" href="css_files/main.css">
+    </head>
+    <body>
+        <?php
+            include "assets/header.php";
+        ?>
+        <div id="maincontent">
+            
+        </div>
+    </body>
+</html>
